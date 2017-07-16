25fd8919f766b5f6dd85254b205531e6908c123e
Neous1 <Neous1@users.noreply.github.com>
2017-07-16 15:46:46 -0400

    res.send , returns error because result is returned as a string

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -3,7 +3,11 @@ var app = express();
 var request = require("request");
 
 app.get("/results", function(req,res){
-    res.send("Hello, it works!");
+    request("http://www.omdbapi.com/?s=star&&apikey=thewdb", function(error, response, body){
+        if(!error && response.statusCode === 200){
+            res.send(body["Search"][0])
+        };
+    });
 });
 
 app.listen(3000, function(){
