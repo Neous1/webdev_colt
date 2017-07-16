cd659ef3f9899c88cf1c146c84167620e45797d7
Neous1 <Neous1@users.noreply.github.com>
2017-07-16 16:44:49 -0400

    add search.ejs to query user for input capture user input and send it to api request

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -4,9 +4,14 @@ var request = require("request");
 //take our result data and send it to a template
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+    res.render("search")
+})
 
 app.get("/results", function(req,res){
-    request("http://www.omdbapi.com/?s=Connecticut&&apikey=thewdb", function(error, response, body){
+    //capture our seach from search.ejs input
+    var query = req.query.search;
+    request("http://www.omdbapi.com/?s=" + query +"&&apikey=thewdb", function(error, response, body){
         if(!error && response.statusCode === 200){
             var data= JSON.parse(body)
             res.render("results", {data:data})
